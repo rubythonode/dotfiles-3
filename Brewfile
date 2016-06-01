35085f1049c7a007bf32f3e86ceca994310f0fa7
appkr <juwonkim@me.com>
2016-06-01 13:24:09 +0900

    new commenting style applied

--- Brewfile
+++ Brewfile
@@ -1,32 +1,55 @@
+##
 # Taps
+##
+
 tap 'caskroom/cask'
 tap 'caskroom/fonts'
 tap 'caskroom/versions'
-# tap 'homebrew/bundle'
 tap 'homebrew/dupes'
 tap 'homebrew/php'
 tap 'homebrew/services'
 
+##
 # Make sure apps get installed in system Applications dir
+##
+
 cask_args appdir: '/Applications'
 
+##
 # Install ZSH
+##
+
 brew 'zsh'
 brew 'zsh-completions'
 
+##
 # Install GNU core utilities (those that come with OS X are outdated)
+##
+
 brew 'coreutils'
 
+##
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+##
+
 brew 'findutils'
 
+##
 # Install Bash 4
+##
+
 brew 'bash'
 
+##
 # Install more recent versions of some OS X tools
+##
+
 brew 'homebrew/dupes/grep'
 
+##
 # Install Binaries
+##
+
 # brew 'awscli'
 # brew 'autoconf'
 brew 'git'
@@ -46,32 +69,45 @@ brew 'tree'
 # brew 'trash'
 brew 'wget'
 
+##
 # Development-PHP
+##
+
 brew 'php70', args: ['without-apache']
 brew 'php70-intl'
 brew 'php70-xdebug'
 brew 'composer'
 brew 'phpunit'
 
+##
 # Development-Ruby
+##
+
 brew 'ruby'
 brew 'rbenv'
 
+##
 # Development-Python (includes pip, easy_install)
+##
+
 brew 'python'
 
+##
 # Development-Database
+##
+
 # brew 'mariadb'
 brew 'mysql'
 brew 'sqlite'
 
+##
 # Apps
-# cask 'alfred'
+##
+
 # cask 'atom'
 cask 'balsamiq-mockups'
 cask 'calibre'
 cask 'dockertoolbox'
-# cask 'dropbox'
 cask 'evernote'
 cask 'firefox'
 # cask 'github-desktop'
@@ -83,7 +119,6 @@ cask 'intellij-idea'
 cask 'iterm2'
 # cask 'mactex'
 cask 'movist'
-# cask 'mysqlworkbench'
 cask 'phpstorm'
 # cask 'screenhero'
 cask 'sequel-pro'
@@ -96,19 +131,24 @@ cask 'sublime-text3'
 cask 'vagrant'
 cask 'virtualbox'
 cask 'virtualbox-extension-pack'
-# cask 'vlc'
 cask 'vmware-fusion7'
 cask 'wireshark'
 # cask 'wkhtmltopdf'
 
+##
 # Quicklook
+##
+
 # cask 'qlcolorcode'
 # cask 'qlmarkdown'
 # cask 'quicklook-json'
 # cask 'quicklook-csv'
 # cask 'qlstephen'
 
+##
 # Fonts
+##
+
 # cask 'font-sauce-code-powerline'
 cask 'font-source-code-pro'
 # cask 'font-source-sans-pro'
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,60 +1,113 @@
 #!/bin/sh
 
+##
+# Thanks to Maxime Fabre! https://speakerdeck.com/anahkiasen/a-storm-homebrewin
+# Thanks to Mathias Bynens! https://mths.be/osx
+##
+
+##
 # Set temporary variable
+##
+
 DOTFILES=$HOME/dotfiles
 
+##
 # Update dotfiles itself first
+##
+
 [ -d "$DOTFILES/.git" ] && git --work-tree="$DOTFILES" --git-dir="$DOTFILES/.git" pull origin master
 
+##
 # Check for Homebrew and install if we don't have it
+##
+
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+##
 # Update Homebrew recipes
+##
+
 brew update
 
+##
 # Install all our dependencies with bundle (See Brewfile)
+##
+
 brew tap homebrew/bundle
 brew bundle --file=$DOTFILES/Brewfile
 brew cleanup
 brew cask cleanup
 
+##
 # Install global Git configuration
+##
+
 [ ! -f $HOME/.gitconfig ] && ln -nfs $DOTFILES/.gitconfig $HOME/.gitconfig
 git config --global core.excludesfile $DOTFILES/.gitignore_global
 
+##
 # Make ZSH the default shell environment
+##
+
 chsh -s $(which zsh)
 
+##
 # Install Oh-my-zsh
+##
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+##
 # Install & execute profile
+##
+
 [ ! -f $HOME/.zshrc ] && ln -nfs $DOTFILES/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
+##
 # Install Mackup cofnig
+##
+
 [ ! -f $HOME/.mackup.cfg ] && ln -nfs $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
 
+##
 # Install global Composer packages
+##
+
 /usr/local/bin/composer global require laravel/installer laravel/envoy laravel/valet tightenco/jigsaw
 
+##
 # Install Laravel Valet
+##
+
 $HOME/.composer/vendor/bin/valet install
 
+##
 # Install Homestead Repo
+##
+
 git clone git@github.com:laravel/homestead.git $HOME/Homestead
 [[ $(basename $(pwd)) == "Homestead" ]] && cd $HOME/Homestead
 vagrant box add laravel/homestead
 
+##
 # Install global Node packages
+##
+
 npm install gitbook-cli gulp-cli gulp yo http-server --global --save
 
+##
 # Install Rails & Jekyll
+##
+
 gem install rails
 gem install jekyll
 
+##
 # Set OS X preferences
 # We will run this last because this will reload the shell
+##
+
 source $DOTFILES/.osx
