619d2c1ba13b5409aff206a3891703800a685e0c
appkr <juwonkim@me.com>
2016-09-07 00:34:19 +0900

    fix bug at conditional

--- bootstrap.sh
+++ bootstrap.sh
@@ -122,7 +122,7 @@ cd $HOME/workspace && $HOME/.composer/vendor/bin/valet park
 
 git clone git@github.com:laravel/homestead.git $HOME/Homestead
 bash $HOME/Homestead/init.sh
-[[ $(basename $(pwd)) == "Homestead" ]] && cd $HOME/Homestead && vagrant box add laravel/homestead
+[[ $(basename $(pwd)) != "Homestead" ]] && cd $HOME/Homestead && vagrant box add laravel/homestead
 
 ##
 # Install global Node packages
